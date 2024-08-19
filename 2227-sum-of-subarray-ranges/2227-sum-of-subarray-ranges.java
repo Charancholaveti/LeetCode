0a3cb2fc20a7905f573e8fdf5750a0e2e49ce467
charancholaveti <161560764+Charancholaveti@users.noreply.github.com>
2024-08-19 16:02:44 +0530

    Time: 20 ms (42.74%), Space: 44.3 MB (53.61%) - LeetHub

--- 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.java
+++ 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.java
@@ -1,14 +1,80 @@
 class Solution {
-    public long subArrayRanges(int[] nums) {
-        long res = 0;
-        for (int i = 0; i < nums.length; i++) {
-            int max = nums[i], min = nums[i];
-            for (int j = i; j < nums.length; j++) {
-                max = Math.max(max, nums[j]);
-                min = Math.min(min, nums[j]);
-                res += max - min;
+    public long sumSubarrayMaxs(int[] arr) {
+        int n=arr.length;
+        int [] left=new int[n];
+        int [] right=new int[n];
+        Arrays.fill(left, -1);
+        Arrays.fill(right, n);
+
+        Deque<Integer> st=new ArrayDeque<>();
+        for(int i=0;i<n;i++){
+            while(!st.isEmpty() && arr[st.peek()]<=arr[i]){
+                st.pop();
             }
+            if(!st.isEmpty()) left[i]=st.peek();
+            st.push(i);
         }
-        return res;
+        st.clear();
+        for(int i=n-1;i>=0;i--){
+            while(!st.isEmpty() && arr[st.peek()]<arr[i]){
+                st.pop();
+            }
+            if(!st.isEmpty()) right[i]=st.peek();
+            st.push(i);
+        }
+        long total=0;
+        int mod=(int)1e9+7;
+        for(int i=0;i<n;i++){
+            total+=(long)(i-left[i])*(right[i]-i)*arr[i];
+            // total %= mod;
+        }
+        return total;
+    } 
+    public long sumSubarrayMins(int[] arr) {
+        int n=arr.length;
+        int [] left=new int[n];
+        int [] right=new int[n];
+        Arrays.fill(left, -1);
+        Arrays.fill(right, n);
+
+        Deque<Integer> st=new ArrayDeque<>();
+        for(int i=0;i<n;i++){
+            while(!st.isEmpty() && arr[st.peek()]>=arr[i]){
+                st.pop();
+            }
+            if(!st.isEmpty()) left[i]=st.peek();
+            st.push(i);
+        }
+        st.clear();
+        for(int i=n-1;i>=0;i--){
+            while(!st.isEmpty() && arr[st.peek()]>arr[i]){
+                st.pop();
+            }
+            if(!st.isEmpty()) right[i]=st.peek();
+            st.push(i);
+        }
+        long total=0;
+        int mod=(int)1e9+7;
+        for(int i=0;i<n;i++){
+            total+=(long)(i-left[i])*(right[i]-i)*arr[i];
+            // total %= mod;
+            // total+=(long)(i-left[i])*(right[i]-i);
+
+        }
+        return total;
+    } 
+    public long subArrayRanges(int[] nums) {
+        // long res = 0;
+        // for (int i = 0; i < nums.length; i++) {
+        //     int max = nums[i], min = nums[i];
+        //     for (int j = i; j < nums.length; j++) {
+        //         max = Math.max(max, nums[j]);
+        //         min = Math.min(min, nums[j]);
+        //         res += max - min;
+        //     }
+        // }
+        // return res;
+        int mod=(int)1e9+9;
+        return (long)(sumSubarrayMaxs(nums)-sumSubarrayMins(nums));
     }
 }
