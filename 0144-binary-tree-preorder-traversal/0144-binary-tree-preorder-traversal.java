1f34bd7131a56a23747805fdda53178cc1c92db3
charancholaveti <161560764+Charancholaveti@users.noreply.github.com>
2024-10-03 18:45:59 +0530

    Time: 0 ms (100%), Space: 41.7 MB (17.11%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -14,19 +14,28 @@
  * }
  */
 class Solution {
-        static void preorder(TreeNode root, List<Integer> arr) {
-        if (root == null) {
-            return;
-        }
-        arr.add(root.val);
-        preorder(root.left, arr);
-        preorder(root.right, arr);
-      }
-
-
+    //     static void preorder(TreeNode root, List<Integer> arr) {
+    //     if (root == null) {
+    //         return;
+    //     }
+    //     arr.add(root.val);
+    //     preorder(root.left, arr);
+    //     preorder(root.right, arr);
+    //   }
     public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> arr = new ArrayList<>();
-        preorder(root, arr);
-        return arr;
+        // List<Integer> arr = new ArrayList<>();
+        // preorder(root, arr);
+        // return arr;
+        List<Integer>al=new ArrayList<>();
+        Stack<TreeNode>st=new Stack<>();
+        if(root==null) return al;
+        st.push(root);
+        while(!st.isEmpty()){
+            TreeNode temp=st.pop();
+            al.add(temp.val);
+            if(temp.right!=null) st.push(temp.right);
+            if(temp.left!=null) st.push(temp.left);
+        }
+        return al;
     }
 }
