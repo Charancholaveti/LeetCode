b4342d8cac3b632fd4078166e56582cf14bbd189
charancholaveti <161560764+Charancholaveti@users.noreply.github.com>
2024-10-04 10:10:39 +0530

    Time: 0 ms (100%), Space: 44.2 MB (95.76%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -13,17 +13,32 @@
  *     }
  * }
  */
+//  public int diameterOfBinaryTree(TreeNode root) {
+//         int []d1=new int[1];
+//         height(root,d1);
+//         return d1[0];
+        
+//     }
+//     public int height(TreeNode root){
+//         if(root==null) return 0;
+//         int left=height(root.left,d1);
+//         int right=height(root.right,d1);
+//         d1[0]=Math.max(d1[0],left+right);
+//         return 1+Math.max(left,right);
+//     }
 class Solution {
-    public int diameterOfBinaryTree(TreeNode root) {
-        int []d1=new int[1];
-        height(root,d1);
-        return d1[0];
-    }
-    public int height(TreeNode root,int [] d1){
+    private static int diameter=0;
+    public static int height(TreeNode root){
         if(root==null) return 0;
-        int left=height(root.left,d1);
-        int right=height(root.right,d1);
-        d1[0]=Math.max(d1[0],left+right);
-        return 1+Math.max(left,right);
+        int lh=height(root.left);
+        int rh=height(root.right);
+        diameter=Math.max(diameter,lh+rh);
+        return Math.max(lh,rh)+1;
+
+    }
+    public int diameterOfBinaryTree(TreeNode root) {
+        diameter=0;
+        height(root);
+        return diameter;
     }
 }
