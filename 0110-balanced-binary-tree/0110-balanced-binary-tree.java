451ee51994fdcd63d5d0de2e6da13186ddd88b56
charancholaveti <161560764+Charancholaveti@users.noreply.github.com>
2024-10-04 10:13:40 +0530

    Time: 0 ms (100%), Space: 44.4 MB (33.78%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -17,7 +17,7 @@ class Solution {
     public static int dfs(TreeNode root){
         if(root==null) return 0;
         int lh=dfs(root.left);
-        if(lh==-1)return -1;
+         if(lh==-1)return -1;
         int rh=dfs(root.right);
         if(rh==-1)return -1;
         if(Math.abs(lh-rh)>1) return -1;
